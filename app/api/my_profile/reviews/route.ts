39c2286c0c4d1ac094b9955a9436e52529bb0e37
Jose Colomer <jococi99@gmail.com>
2023-09-13 19:16:51 +0200

    adding user reviews page

---
+++ app/api/my_profile/reviews/route.ts
@@ -0,0 +1,21 @@
+import prismaClient from "@/lib/prisma-client";
+import { NextRequest, NextResponse } from "next/server";
+import { useId } from "react";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const userId = searchParams.get("userId");
+  try {
+    if (userId) {
+      const reviews = await prismaClient.review.findMany({
+        where: { user_id: userId },
+        include: { film: true },
+        orderBy: { created_at: "desc" },
+      });
+      console.log(reviews);
+      return NextResponse.json({ reviews });
+    }
+  } catch (error) {
+    return NextResponse.error();
+  }
+}
--- components/MyUserListsContent.tsx
+++ components/MyUserListsContent.tsx
@@ -15,7 +15,7 @@ export default function MyUserListsComponent({ userId }: Props) {
           setLists(lists);
         }
       });
-  });
+  }, []);
 
   return (
     <>
