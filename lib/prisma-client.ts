c760690ad50019ca4e4868042cd9ca1e39d06bf7
Jose Colomer <jococi99@gmail.com>
2023-09-25 19:38:24 +0200

    changing database for deploy

--- lib/prisma-client.ts
+++ lib/prisma-client.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = new PrismaClient();
-
+const prismaClient = global.prismadb || new PrismaClient();
+if (process.env.NODE_ENV === "production") global.prismadb = prismaClient;
 export default prismaClient;
